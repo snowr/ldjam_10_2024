ec6fa00541f7a4061a9688c9f845995e641bb98c
Sina Nowroozi <sina.nowroozi@gov.ab.ca>
2024-10-06 11:45:13 -0700

    .

--- Gun.cs
+++ Gun.cs
@@ -2,76 +2,76 @@ using Godot;
 
 namespace ldjam_2024
 {
-    [Tool]
-    public class Gun : Node2D
-    {
-        private bool _isFiring;
-        private ulong _lastFireTime;
-        private AnimatedSprite _sprite;
-        [Export] public PackedScene ProjectileScene { get; set; }
-        [Export] public Vector2 MuzzlePos { get; set; }
+	[Tool]
+	public class Gun : Node2D
+	{
+		private bool _isFiring;
+		private ulong _lastFireTime;
+		private AnimatedSprite _sprite;
+		[Export] public PackedScene ProjectileScene { get; set; }
+		[Export] public Vector2 MuzzlePos { get; set; }
 
-        [Export] public float RateOfFire { get; set; } = 1000f; // milliseconds
+		[Export] public float RateOfFire { get; set; } = 1000f; // milliseconds
 
-        [Export] public NodePath SpritePath { get; set; }
-        public bool PlayerOwned { get; set; }
+		[Export] public NodePath SpritePath { get; set; }
+		public bool PlayerOwned { get; set; }
 
-        public override void _Ready()
-        {
-            if (SpritePath == null)
-                return;
+		public override void _Ready()
+		{
+			if (SpritePath == null)
+				return;
 
-            _sprite = GetNode<AnimatedSprite>(SpritePath);
-            if (_sprite == null)
-                GD.PushError("Failed to load sprite for Gun. You idiot.");
-            else
-                _sprite.Connect("animation_finished", this, nameof(OnAnimationFinished));
-        }
+			_sprite = GetNode<AnimatedSprite>(SpritePath);
+			if (_sprite == null)
+				GD.PushError("Failed to load sprite for Gun. You idiot.");
+			else
+				_sprite.Connect("animation_finished", this, nameof(OnAnimationFinished));
+		}
 
-        public bool CanFire()
-        {
-            var time = OS.GetTicksMsec();
-            if (_lastFireTime == 0)
-                return true;
-            return _isFiring == false && time - _lastFireTime >= RateOfFire;
-        }
+		public bool CanFire()
+		{
+			var time = OS.GetTicksMsec();
+			if (_lastFireTime == 0)
+				return true;
+			return _isFiring == false && time - _lastFireTime >= RateOfFire;
+		}
 
-        public void Fire()
-        {
-            if (CanFire())
-            {
-                _isFiring = true;
-                _lastFireTime = OS.GetTicksMsec();
-                if (_sprite == null) return;
-                _sprite.Play("fire");
-                if (ProjectileScene != null)
-                {
-                    var direction = GlobalTransform.x;
-                    GD.Print(
-                        $"Firing ============ {direction} | {direction.Normalized()} | {GetParent<Node2D>().Transform.x}");
-                    ProjectileManager.Instance.SpawnProjectile(ProjectileScene, ToGlobal(MuzzlePos), direction,
-                        PlayerOwned, this);
-                }
-            }
-            else
-            {
-                var t = OS.GetTicksMsec() - _lastFireTime >= RateOfFire;
-                GD.Print($"Cant Fire {_isFiring == false}  {t} {OS.GetTicksMsec()} {_lastFireTime} {RateOfFire}");
-            }
-        }
+		public void Fire()
+		{
+			if (CanFire())
+			{
+				_isFiring = true;
+				_lastFireTime = OS.GetTicksMsec();
+				if (_sprite == null) return;
+				_sprite.Play("fire");
+				if (ProjectileScene != null)
+				{
+					var direction = GlobalTransform.x;
+					GD.Print(
+						$"Firing ============ {direction} | {direction.Normalized()} | {GetParent<Node2D>().Transform.x}");
+					ProjectileManager.Instance.SpawnProjectile(ProjectileScene, ToGlobal(MuzzlePos), direction,
+						PlayerOwned, this);
+				}
+			}
+			else
+			{
+				var t = OS.GetTicksMsec() - _lastFireTime >= RateOfFire;
+				GD.Print($"Cant Fire {_isFiring == false}  {t} {OS.GetTicksMsec()} {_lastFireTime} {RateOfFire}");
+			}
+		}
 
-        private void OnAnimationFinished()
-        {
-            if (_sprite.Animation == "fire")
-            {
-                _isFiring = false;
-                _sprite.Play("idle");
-            }
-        }
+		private void OnAnimationFinished()
+		{
+			if (_sprite.Animation == "fire")
+			{
+				_isFiring = false;
+				_sprite.Play("idle");
+			}
+		}
 
-        public override void _EnterTree()
-        {
-            Update();
-        }
-    }
+		public override void _EnterTree()
+		{
+			Update();
+		}
+	}
 }
