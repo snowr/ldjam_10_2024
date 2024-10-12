6043db3cab0c6f3fde856a42088c145db1fd5d39
Sina Nowroozi <sina.nowroozi@gov.ab.ca>
2024-10-12 15:19:25 -0700

    shotgun spread

--- Shotgun.cs
+++ Shotgun.cs
@@ -2,8 +2,51 @@ using Godot;
 
 namespace ldjam_2024
 {
-	[Tool]
-	public class Shotgun : Gun
-	{
-	}
+    [Tool]
+    public class Shotgun : Gun
+    {
+        public override void Fire()
+        {
+            if (CanFire())
+            {
+                _isFiring = true;
+                _lastFireTime = OS.GetTicksMsec();
+                if (_sprite == null) return;
+                _sprite.Play("fire");
+                Vector2 muzzleOffset = new Vector2(0, 5);
+                if (ProjectileScene != null)
+                {
+                    Transform2D currTransform = GlobalTransform;
+                    Vector2 direction = (currTransform.x).Normalized();
+                    float rotationAngle = 15;
+                    Vector2 direction2 = direction.Rotated(Mathf.Deg2Rad(rotationAngle));
+                    Vector2 direction3 = direction.Rotated(Mathf.Deg2Rad(-rotationAngle));
+
+                    // If facing left
+                    if (direction.x < 0)
+                    {
+                        direction2 = direction.Rotated(Mathf.Deg2Rad(-rotationAngle));
+                        direction3 = direction.Rotated(Mathf.Deg2Rad(rotationAngle));
+                    }
+
+                    Vector2 muzzleGlobal = ToGlobal(MuzzlePos);
+
+                    ProjectileManager.Instance.SpawnProjectile(ProjectileScene,
+                        muzzleGlobal, direction,
+                        PlayerOwned, this);
+                    ProjectileManager.Instance.SpawnProjectile(ProjectileScene,
+                        muzzleGlobal + muzzleOffset, direction2,
+                        PlayerOwned, this);
+                    ProjectileManager.Instance.SpawnProjectile(ProjectileScene,
+                        muzzleGlobal - muzzleOffset, direction3,
+                        PlayerOwned, this);
+                }
+            }
+            else
+            {
+                var t = OS.GetTicksMsec() - _lastFireTime >= RateOfFire;
+                // GD.Print($"Cant Fire {_isFiring == false}  {t} {OS.GetTicksMsec()} {_lastFireTime} {RateOfFire}");
+            }
+        }
+    }
 }
